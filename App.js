865daf9988a69b406b7c8a033a87b3a627ba52d4
CristiManolescu <m_cristian_s96@yahoo.com>
2023-08-08 12:08:34 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "Asta e un H1"),
+    React.createElement("h2", {}, "Asta e un H2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Asta e un H1"),
+    React.createElement("h2", {}, "Asta e un H2"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
