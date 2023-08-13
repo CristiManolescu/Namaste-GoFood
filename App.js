8c199a0b49371e08146e9a354183bcf6d2417fd9
CristiManolescu <m_cristian_s96@yahoo.com>
2023-08-13 16:55:42 +0300

    First Part

--- App.js
+++ App.js
@@ -25,7 +25,15 @@ const Header = () => {
 const RestaurantCard = () => {
   return (
     <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://www.kfc.ro/uploads/produse/smart-choice/mare_snack-box-1619164897.jpg"
+      />
       <h3>KFC</h3>
+      <h4>Crispy, More Crispy</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
     </div>
   );
 };
@@ -35,6 +43,9 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
         <RestaurantCard />
       </div>
     </div>
