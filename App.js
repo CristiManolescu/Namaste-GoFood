c2e73f5345069a6ee9d4d64934b17527181f85ac
CristiManolescu <m_cristian_s96@yahoo.com>
2023-08-14 12:14:45 +0300

    Dynamic Cards

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ const Header = () => {
   );
 };
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
   return (
     <div className="res-card">
       <img
@@ -30,8 +30,8 @@ const RestaurantCard = () => {
         alt="res-logo"
         src="https://www.kfc.ro/uploads/produse/smart-choice/mare_snack-box-1619164897.jpg"
       />
-      <h3>KFC</h3>
-      <h4>Crispy, More Crispy</h4>
+      <h3>{props.resName}</h3>
+      <h4>{props.cuisine}</h4>
       <h4>4.4 stars</h4>
       <h4>38 minutes</h4>
     </div>
@@ -43,10 +43,8 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
+        <RestaurantCard resName="Indian Restaurant" cuisine="India" />
+        <RestaurantCard resName="KFC" cuisine="America" />
       </div>
     </div>
   );
