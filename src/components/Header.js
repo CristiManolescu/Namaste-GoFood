b845b6743c3782f74aeba70e062611e2e47caba9
CristiManolescu <m_cristian_s96@yahoo.com>
2023-08-27 17:00:46 +0300

    Add Link Component for routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,10 @@
 import { LOGO_URL } from "../utils/constants";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [btnLog, setBtnLog] = useState("Login");
   console.log("Header rendered");
-
-  useEffect(() => {
-    console.log("btnLog si-a schimbat valoarea");
-  }, [btnLog]);
 
   return (
     <div className="header">
@@ -16,9 +13,15 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
           <li>Cart</li>
           <button
             className="login"
