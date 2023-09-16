5e9eebfa4d44992600e24e6f87e8dd07c2b11459
CristiManolescu <m_cristian_s96@yahoo.com>
2023-09-16 13:30:31 +0300

    Add: Some Unit Testcases for the Contact Component

---
+++ src/components/__test__/Contact.test.js
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import Contact from "../Contact";
+import "@testing-library/jest-dom";
+
+test("Should load contact us component", () => {
+  render(<Contact />);
+
+  const heading = screen.getByRole("heading");
+
+  //Assertion
+  expect(heading).toBeInTheDocument();
+});
+
+test("Should button inside contact us component", () => {
+  render(<Contact />);
+
+  const button = screen.getByRole("button");
+
+  //Assertion
+  expect(button).toBeInTheDocument();
+});
+
+test("Should placeholder inside contact us component", () => {
+  render(<Contact />);
+
+  const placeholder = screen.getByPlaceholderText("name");
+
+  //Assertion
+  expect(placeholder).toBeInTheDocument();
+});
+
+test("Should load 2 input boxes on the Contact component", () => {
+  render(<Contact />);
+
+  const inputBoxes = screen.getAllByRole("textbox"); //All is used for multiple elements
+
+  //Assertion
+  expect(inputBoxes.length).toBe(2);
+});
