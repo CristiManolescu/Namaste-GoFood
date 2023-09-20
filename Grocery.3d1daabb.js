21a81d99119502a1280d376b81fdc04200016050
CristiManolescu <m_cristian_s96@yahoo.com>
2023-09-20 15:39:10 +0300

    Deploy React app to GitHub Pages

---
+++ Grocery.3d1daabb.js
@@ -0,0 +1,2 @@
+var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire164e;e.register("7kZKn",function(n,o){Object.defineProperty(n.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(n.exports,"default",{get:function(){return t},set:void 0,enumerable:!0,configurable:!0});var i=e("5aRSq"),t=()=>(0,i.jsx)("h1",{children:"This is a giant page with lots of components"})});
+//# sourceMappingURL=Grocery.3d1daabb.js.map
---
+++ Grocery.56d8deaf.js
@@ -0,0 +1,2 @@
+var e;(e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire164e).register("dhpq2",function(n,o){Object.defineProperty(n.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(n.exports,"default",{get:function(){return t},set:void 0,enumerable:!0,configurable:!0});var i=e("cc6u1"),t=function(){return(0,i.jsx)("h1",{children:"This is a giant page with lots of components"})}});
+//# sourceMappingURL=Grocery.56d8deaf.js.map
